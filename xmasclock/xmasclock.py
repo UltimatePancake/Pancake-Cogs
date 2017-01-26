612011f8751cdfd6c2af8c2a7774ab87646b65ca
Pier Gaetani <pier.gaetani@gmail.com>
2017-01-26 17:02:28 -0600

    difference display

--- xmasclock/xmasclock.py
+++ xmasclock/xmasclock.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import date
 import discord
 from discord.ext import commands
 
@@ -12,10 +13,13 @@ class xmasclock:
     async def xmasclock(self):
         """Display days left 'til xmas"""
 
-        #code
-        await self.bot.say("I can do stuff!")
         now = datetime.datetime.now()
-        await self.bot.say(now.isoformat())
+        today = date(now.year, now.month, now.day)
+        xmasday = date(now.year, 12, 25)
+
+        delta = today - xmasday
+
+        await self.bot.say(delta.days)
 
 
 def setup(bot):
