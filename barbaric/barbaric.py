5f59e549cc4a2493ef93bc9bd216042c502cfd87
Pier Gaetani <pier.gaetani@gmail.com>
2017-01-27 18:31:52 -0600

    barbaric initial commit

---
+++ barbaric/barbaric.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+
+class barbaric:
+    """Absolutely"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.image = "data/barbaric/barbaric.jpg"
+
+    @commands.command(pass_context=True)
+    async def barbaric(self, ctx):
+        """Barbaric"""
+
+        # code
+        channel = ctx.message.channel
+        await self.bot.send_file(channel, self.image)
+
+    async def listener(self, message):
+        if message.author.id != self.bot.user.id:
+            if message.content.lower().contains('barbaric'):
+                await self.bot.send_file(channel, self.image)
+
+
+def setup(bot):
+    n = barbaric(bot)
+    bot.add_listener(n.listener, "on_message")
+    bot.add_cog(n)
