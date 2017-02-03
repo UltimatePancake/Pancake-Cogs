62b2a4533242aee019204b091adff8466cd7d287
UltimatePancake <pier.gaetani@gmail.com>
2017-02-03 16:35:58 -0600

    xkcd cog initial commit

---
+++ xkcd/xkcd.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import xkcd
+from __main__ import send_cmd_help
+
+
+class XKCD:
+    """xkcd comic cog."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(name="xkcd", pass_context=True)
+    async def xkcd(self, ctx):
+        """Displays latest xkcd comic."""
+        if ctx.invoked_subcommand is None:
+            await self.bot.say(xkcd.getLatestComic().getImageLink())
+
+    @xkcd.command(name="random", pass_context=True)
+    async def _random(self, ctx):
+        """Displays random xkcd comic."""
+        await self.bot.say(xkcd.getRandomComic().getImageLink())
+
+    @xkcd.command(name="number", pass_context=True)
+    async def _number(self, ctx, number: int):
+        """Displays specified xkcd comic."""
+        await self.bot.say(xkcd.getComic(number).getImageLink())
+
+
+def setup(bot):
+    bot.add_cog(XKCD(bot))
